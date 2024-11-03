f0e87863e92adbfcb26edef1ec3b552e05daed05
deprese <martinhublar1@gmail.com>
2024-11-03 22:03:07 +0100

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -2,44 +2,42 @@ import time
 import tkinter
 from tkinter import ttk
 
-class App():
-    def __init__(self):
-        self.root = tkinter.Tk()
-        self.root.title("Vojtik program")
-        self.root.geometry("400x200")
-        self.mainframe = tkinter.Frame(self.root, background="light gray")
-        self.mainframe.pack(fill="both", expand=True)
-        
-        self.text = ttk.Label(self.mainframe, text="Hello World", font=("Brass Mono", 30))
-        self.text.grid(row=0, column=0, pady=20)
+def set_text():
+    new_text = set_text_field.get()
+    text.config(text=new_text)
 
-        self.set_text_field = ttk.Entry(self.mainframe, width=50)
-        self.set_text_field.grid(row=1, column=0, sticky="NWES")
-        self.set_text_button = ttk.Button(self.mainframe, text="Set text", command=self.set_text)
-        self.set_text_button.grid(row=1, column=1)
+def set_color():
+    new_color = set_color_field.get()
+    text.config(foreground=new_color)
 
-        color_options = ["red", "blue", "white", "gray", "black"]
-        self.set_color_field = ttk.Combobox(self.mainframe, width=50, values=color_options)
-        self.set_color_field.grid(row=2, column=0, pady=10, sticky="NWES")
-        self.set_text_button = ttk.Button(self.mainframe, text="Set color", command=self.set_color)
-        self.set_text_button.grid(row=2, column=1, pady=10)    
+def reverse():
+    text.config(text="Hello World", foreground="black")
 
-        self.set_reverse_button = ttk.Button(self.mainframe, text="Reverse changes", command=self.reverse)
-        self.set_reverse_button.grid(row=3, column=0, sticky="NWES")
+root = tkinter.Tk()
+root.title("Vojtik program")
+root.geometry("400x200")
+mainframe = tkinter.Frame(root, background="light gray")
+mainframe.pack(fill="both", expand=True)
 
-        self.root.mainloop()
-        return
+text = ttk.Label(mainframe, text="Hello World", font=("Brass Mono", 30))
+text.grid(row=0, column=0, pady=20)
 
-    def set_text(self):
-        new_text = self.set_text_field.get()
-        self.text.config(text=new_text)
+set_text_field = ttk.Entry(mainframe, width=50)
+set_text_field.grid(row=1, column=0, sticky="NWES")
+set_text_button = ttk.Button(mainframe, text="Set text", command=set_text)
+set_text_button.grid(row=1, column=1)
 
-    def set_color(self):
-        new_color = self.set_color_field.get()
-        self.text.config(foreground=new_color)
+color_options = ["red", "blue", "white", "gray", "black"]
+set_color_field = ttk.Combobox(mainframe, width=50, values=color_options)
+set_color_field.grid(row=2, column=0, pady=10, sticky="NWES")
+set_text_button = ttk.Button(mainframe, text="Set color", command=set_color)
+set_text_button.grid(row=2, column=1, pady=10)    
 
-    def reverse(self):
-        self.text.config(text="Hello World", foreground="black")
+set_reverse_button = ttk.Button(mainframe, text="Reverse changes", command=reverse)
+set_reverse_button.grid(row=3, column=0, sticky="NWES")
+
+root.mainloop()
+
 
 def countdown(time_sec):
     while time_sec:
@@ -49,9 +47,9 @@ def countdown(time_sec):
         time.sleep(1)
         time_sec -= 1
 
-    print("stop")
+print("stop")
 
 if __name__ == "__main__":
-    App()
+    __init__()
     #number = int(input("enter a number "))
     #countdown(number * 60)
